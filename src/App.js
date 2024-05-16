b44fee799855a9f13bb4ba930c743cfc2990c5db
Thai Bao <boishiro2002@gmail.com>
2024-05-16 11:36:59 +0700

    news

--- src/App.js
+++ src/App.js
@@ -6,45 +6,8 @@ import Main from './Compernents/Main/Main'
 import Footer from './Compernents/Footer/Footer'
 import Memory from './Compernents/Memory/Memory';
 function App() {
-  // const [info, setInfo] =  useState({
-  //     fullName: "" ,
-  //     age : 32
-  // })
-  // const [gift, setGift] = useState()
-  // const gifs = [
-  //   'Bánh mì', 
-  //   'Sữa',
-  //   'Kẹo'
-  // ]
-  // const fullName = ["Thái Quang Bảo", "Đức", "Tuấn Anh"]
-  // const handleUpdate = () => {
-  //   const index = Math.floor(Math.random()*gifs.length)
-  //   const indexN = Math.floor(Math.random()*fullName.length)
-  //   info.fullName = fullName[indexN];
-  //   if(info.fullName !== "Thái Quang Bảo") {
-  //     setInfo(pre => ({
-  //       fullName: info.fullName,
-  //       error : "Bạn không thuộc trong danh sách"
-  //     }))
-  //     setGift("")
-  //   }
-  //   else if (info.fullName === "Thái Quang Bảo") {
-  //     setInfo(pre => (
-  //     {
-  //       fullName: info.fullName,
-  //       describe: "Hello World"
-  //     }))
-  //     setGift(gifs[index])
-  //   }
-   
-  // }
+
   return (
-      // <div className='App' style={{ padding: 20 }}>
-      //   <h1>{JSON.stringify(info)}</h1>
-      //   <h1>{gift || "Chưa được quà"}</h1>
-      //   <button onClick={handleUpdate}>Update</button>
-        
-      // </div>
       <>
         <Navbar />
         <Header />
